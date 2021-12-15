e0405c4e8cfe07533c7fb8d2d861a23c2a7d5356
Zahiruddin Zainal <zahirzainal869@gmail.com>
2021-12-15 15:16:44 +0800

    tambah api geocode, timestamp

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,6 +12,10 @@
             margin-top: 15px;
         }
 
+        .spacing-medium {
+            margin-top: 25px;
+        }
+
         .spacing-large {
             margin-top: 50px;
         }
@@ -33,30 +37,90 @@
                                     <span class="form__btn-text">All satelites API</span>
                                 </a>
                             </div>
-                            <div class="form__group">
+                            <p>To get all satellites list and it's id. For now the server only provide single satelite
+                                which is ISS. The default id is '25544'. You need to go to the following url endpoint:
+                            </p>
+                            <p class="spacing">Example of API usage: </p>
+                            <p class="spacing"><strong>GET: <a
+                                        href="http://api.zvhir.com/api/satelites">http://api.zvhir.com/api/satelites</a></strong>
+                            </p>
+                            <img class="spacing" src="snippet1.png" width="100%" height="auto">
+
+                            <p class="spacing-large" style="margin: auto;">
+                                ---------------------------------------------------------------------</p>
+
+                            <div class="form__group spacing-large">
                                 <a target="_blank" href="/demo/timestamp" class="form__btn" type="submit">
                                     <span class="form__btn-text">ISS LOCATOR<br>(by timestamp)</span>
                                 </a>
                             </div>
-                            <div class="form__group">
+                            <p>You can get information of the satelites at given timestamp. You need to go to the
+                                following url endpoint and pass satelite id and the timestamp: </p>
+                            <p class="spacing">Example of API usage: </p>
+                            <p class="spacing"><strong>GET: <a
+                                        href="http://api.zvhir.com/api/satelite/25544/timestamp/1639499100">http://api.zvhir.com/api/satelite/25544/timestamp/1639499100</a></strong>
+                            </p>
+                            <img class="spacing" src="snippet2.png" width="100%" height="auto">
+
+                            <p class="spacing-large" style="margin: auto;">
+                                ---------------------------------------------------------------------</p>
+
+                            <div class="form__group spacing-large">
                                 <a target="_blank" href="/demo/geocode" class="form__btn" type="submit">
-                                    <span class="form__btn-text" style="margin: auto;">ISS LOCATOR<br>(by geolocation)</span>
+                                    <span class="form__btn-text" style="margin: auto;">ISS LOCATOR<br>(by
+                                        geolocation)</span>
                                 </a>
                             </div>
-                            <div class="form__group">
+                            <p>You can also get reverse information of the satelites by passing the latitude and
+                                longitude: </p>
+                            <p class="spacing">Example of API usage: </p>
+                            <p class="spacing"><strong>GET: <a
+                                        href="http://api.zvhir.com/api/satelite/25544/latitude/4.2105/longitude/101.9758">http://api.zvhir.com/api/satelite/25544/latitude/4.2105/longitude/101.9758</a></strong>
+                            </p>
+                            <img class="spacing" src="snippet5.png" width="100%" height="auto">
+
+                            <p class="spacing-large" style="margin: auto;">
+                                ---------------------------------------------------------------------</p>
+
+
+                            <div class="form__group spacing-large">
                                 <a target="_blank" href="/demo/before" class="form__btn" type="submit">
-                                    <span class="form__btn-text" style="margin: auto;">ISS LOCATOR<br>(before every 10 minutes)</span>
+                                    <span class="form__btn-text" style="margin: auto;">ISS LOCATOR<br>(before every 10
+                                        minutes)</span>
                                 </a>
                             </div>
-                            <div class="form__group">
+                            <p>You can also get information of the satelites every 10 minutes before a given timestamp.
+                                You need to go to the following url endpoint and pass satelite id and the timestamp:
+                            </p>
+                            <p class="spacing">Example of API usage: </p>
+                            <p class="spacing"><strong>GET: <a
+                                        href="http://api.zvhir.com/api/before/satelite/25544/timestamp/1639499100">http://api.zvhir.com/api/before/satelite/25544/timestamp/1639499100</a></strong>
+                            </p>
+                            <img class="spacing" src="snippet3.png" width="100%" height="auto">
+
+                            <p class="spacing-large" style="margin: auto;">---------------------------------------------------------------------</p>
+
+                            <div class="form__group spacing-large">
                                 <a target="_blank" href="/demo/after" class="form__btn" type="submit">
-                                    <span class="form__btn-text" style="margin: auto;">ISS LOCATOR<br>(after every 10 minutes)</span>
+                                    <span class="form__btn-text" style="margin: auto;">ISS LOCATOR<br>(after every 10
+                                        minutes)</span>
                                 </a>
                             </div>
-                            <div class="form__group">
-                                <a target="_blank" href="https://github.com/zahiruddinzainal" class="form__btn"
+                            <p>You can also get information of the satelites every 10 minutes after a given timestamp.
+                                You need to go to the following url endpoint and pass satelite id and the timestamp:
+                            </p>
+                            <p class="spacing">Example of API usage: </p>
+                            <p class="spacing"><strong>GET: <a
+                                        href="http://api.zvhir.com/api/after/satelite/25544/timestamp/1639499100">http://api.zvhir.com/api/after/satelite/25544/timestamp/1639499100</a></strong>
+                            </p>
+                            <img class="spacing" src="snippet4.png" width="100%" height="auto">
+
+                            <p class="spacing-large" style="margin: auto;">---------------------------------------------------------------------</p>
+
+                            <div class="form__group spacing-large">
+                                <a target="_blank" href="https://interview.zvhir.com" class="form__btn"
                                     type="submit">
-                                    <span class="form__btn-text">API DOCUMENTATION</span>
+                                    <span class="form__btn-text">LIVE DEMO</span>
                                 </a>
                             </div>
 
--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ Route::get('satelites', function() {
 //API FOR ISS INFO BY TIMESTAMP 
 Route::get('satelite/{id}/timestamp/{timestamp}', function($id, $timestamp) {
     $url = new \GuzzleHttp\Client();
-    $response = $url->get('https://api.wheretheiss.at/v1/satellites/'. $id .'/positions?timestamps='. $timestamp);
+    $response = $url->get('https://api.wheretheiss.at/v1/satellites/'. $id .'/positions?timestamps='. strtotime($timestamp));
     $location = json_decode($response->getBody(), true);
     return $location;
 });
@@ -29,28 +29,35 @@ Route::get('satelite/{id}/latitude/{latitude}/longitude/{longitude}', function($
 
 //API FOR LOCATIONS AFTER 10 MINUTES
 Route::get('/after/satelite/{id}/timestamp/{timestamp}', function($id, $timestamp) {
-    $locations_before = collect();
     $locations_after = collect();
-    $times_before = collect();
     $times_after = collect();
-    $date = new \DateTime(strtotime($timestamp));
     $round= 10;
-    for($count = 0; $count < $round ; $count++){
-        $date->modify("+10 minutes");
+    $round_count = 0;
+    $date = date('H:i m/d/Y', $timestamp);
+    for($count = 0; $count < $round ; $count++)
+    {
+        $round_count = $round_count + 10;
+        $repeat = strtotime("+10 minutes",strtotime($date));
+        $date = date('H:i m/d/Y ', $repeat);
+        $timestamp = strtotime($date);
         $times_after->push([
-            'masa' => $date->format('H:i A, j F Y'),
-            'timestamp' => strtotime($date->format('H:i:s')),
+            'masa' => $date,
+            'round' => $round_count,
+            'timestamp' => $timestamp,
         ]);
     }
     foreach($times_after as $after){
         $url = new \GuzzleHttp\Client();
         $response = $url->get('https://api.wheretheiss.at/v1/satellites/25544/positions?timestamps='. $after['timestamp']);
         $location = json_decode($response->getBody(), true);
+        $longitude = $location[0]['longitude'];
+        $latitude = $location[0]['latitude'];
         $locations_after->push([
             'masa' => $after['masa'],
+            'round' => $after['round'],
             'timestamp' => $after['timestamp'],
-            'longitude' => $location[0]['longitude'],
-            'latitude' => $location[0]['latitude'],
+            'longitude' => $longitude,
+            'latitude' => $latitude,
         ]);
     }
     return $locations_after;
@@ -59,16 +66,20 @@ Route::get('/after/satelite/{id}/timestamp/{timestamp}', function($id, $timestam
 //API FOR LOCATIONS BEFORE 10 MINUTES
 Route::get('/before/satelite/{id}/timestamp/{timestamp}', function($id, $timestamp) {
     $locations_before = collect();
-    $locations_after = collect();
     $times_before = collect();
-    $times_after = collect();
-    $date = new \DateTime(strtotime($timestamp));
     $round= 10;
-    for($count = 0; $count < $round ; $count++){
-        $date->modify("-10 minutes");
+    $round_count = 0;
+    $date = date('H:i m/d/Y', $timestamp);
+    for($count = 0; $count < $round ; $count++)
+    {
+        $round_count = $round_count - 10;
+        $repeat = strtotime("-10 minutes",strtotime($date));
+        $date = date('H:i m/d/Y ', $repeat);
+        $timestamp = strtotime($date);
         $times_before->push([
-            'masa' => $date->format('H:i A, j F Y'),
-            'timestamp' => strtotime($date->format('H:i:s')),
+            'masa' => $date,
+            'round' => $round_count,
+            'timestamp' => $timestamp,
         ]);
     }
     foreach($times_before as $before){
@@ -79,6 +90,7 @@ Route::get('/before/satelite/{id}/timestamp/{timestamp}', function($id, $timesta
         $latitude = $location[0]['latitude'];
         $locations_before->push([
             'masa' => $before['masa'],
+            'round' => $before['round'],
             'timestamp' => $before['timestamp'],
             'longitude' => $longitude,
             'latitude' => $latitude,
